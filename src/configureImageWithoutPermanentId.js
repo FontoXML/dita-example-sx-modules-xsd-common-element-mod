2f93665bb1f7ad365b6403afd157d3e6afc8a3cc
eva <eva.de.haas@liones.nl>
2019-06-28 10:56:31 +0200

    Upgrade to SDK 7.7

--- src/configureImageWithoutPermanentId.js
+++ src/configureImageWithoutPermanentId.js
@@ -1,28 +1,26 @@
-define([
-	'fontoxml-families/configureAsImageInFrame',
-	'fontoxml-families/configureAsInlineImageInFrame'
-], function (
-	configureAsImageInFrame,
-	configureAsInlineImageInFrame
-) {
-	'use strict';
+import configureAsImageInFrame from 'fontoxml-families/src/configureAsImageInFrame.js';
+import configureAsInlineImageInFrame from 'fontoxml-families/src/configureAsInlineImageInFrame.js';
 
-	// This file removes the permanentId which is the other way around then for cross reference,
-	// for compatability reasons.
-	return function configureImageWithoutPermanentId (sxModule) {
-		// To disable permanentId's in images (also known as the reference pipeline), set
-		// isPermanentId to false.
+// This file removes the permanentId which is the other way around then for cross reference,
+// for compatability reasons.
+export default function configureImageWithoutPermanentId(sxModule) {
+	// To disable permanentId's in images (also known as the reference pipeline), set
+	// isPermanentId to false.
 
-		configureAsImageInFrame(sxModule, 'self::image', undefined, {
+	configureAsImageInFrame(sxModule, 'self::image', undefined, {
+		priority: 2,
+		referenceQuery: '@href',
+		isPermanentId: false
+	});
+
+	configureAsInlineImageInFrame(
+		sxModule,
+		'self::image and fonto:in-inline-layout(.)',
+		undefined,
+		{
 			priority: 2,
 			referenceQuery: '@href',
 			isPermanentId: false
-		});
-
-		configureAsInlineImageInFrame(sxModule, 'self::image and fonto:in-inline-layout(.)', undefined, {
-			priority: 2,
-			referenceQuery: '@href',
-			isPermanentId: false
-		});
-	};
-});
+		}
+	);
+}
